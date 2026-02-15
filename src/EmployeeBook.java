d49dea2c2e99e162acbbfa9839456a2a12d96c0e
yarmoshchuk-E <yarmoshchuk_80@mail.ru>
2026-02-16 02:27:58 +0400

    на всякий пожарный

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -16,6 +16,7 @@ public class EmployeeBook {
                 return true;
             }
         }
+        System.out.println("для сотрудника "+employee+ " в базе нет мест");
         return false;
     }
 
@@ -52,7 +53,7 @@ public class EmployeeBook {
                 for (Employee emp : employees) {
                     if (emp != null) {
                         tax = emp.getSalary() * 0.13;
-                        System.out.printf(emp.getFullName() + "%.2f.%n",tax);
+                        System.out.printf(emp.getFullName() +" сумма налога "+ "%.2f.%n",tax);
                     }
                 }
                 break;
@@ -61,19 +62,94 @@ public class EmployeeBook {
                     if (emp != null) {
                         if (emp.getSalary() > 0 && emp.getSalary() <= 150) {
                             tax = emp.getSalary() * 0.13;
-                            System.out.printf(emp.getFullName() + "%.2f.%n",tax);
+                            System.out.printf(emp.getFullName() +" сумма налога "+ "%.2f.%n",tax);
 
                         } else if (emp.getSalary() > 150 && emp.getSalary() <= 350) {
                             tax = emp.getSalary() * 0.17;
-                            System.out.printf(emp.getFullName() + "%.2f.%n",tax);
+                            System.out.printf(emp.getFullName() +" сумма налога "+ "%.2f.%n",tax);
 
                         } else if (emp.getSalary() > 350) {
                             tax = emp.getSalary() * 0.21;
-                            System.out.printf(emp.getFullName() + "%.2f.%n",tax);
+                            System.out.printf(emp.getFullName() +" сумма налога "+"%.2f.%n",tax);
                         }
                     }
                 }
                 break;
         }
     }
+
+    //создаю метод для индексации зарплат сотрудников по номеру отдела
+    public static void indexTheSalary(int departmentNumber, int indexSalary) {
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getDepartment() != departmentNumber) {
+                    continue;
+                }
+                int newSalary;
+                newSalary =emp.getSalary()+ emp.getSalary() * indexSalary / 100;
+                emp.setSalary(newSalary);
+                System.out.println(emp.getFullName() + newSalary);
+            }
+        }
+    }
+
+    public static void printFirstEmployeeMaxSalary(int department, int salaryForComparison) {
+        for (int i = 0; i < employees.length; i++) {
+            Employee emp = employees[i];
+            if (emp != null && emp.getDepartment() == department && emp.getSalary() > salaryForComparison) {
+                System.out.print("№ "+emp.getId()+" ");
+                emp.printShortInfo();
+                break;
+            }
+        }
+    }
+
+    public static void getEmployeesSalaryLowWage(int wage, int employeeNumber) {
+        int count = 0;
+        int i = 0;
+        while (count <= employeeNumber && i < employees.length) {
+            Employee emp = employees[i];
+            if (emp == null) {
+                break;
+            }
+            if (count == employeeNumber) {
+                break;
+            }
+            if (emp != null && emp.getSalary() < wage) {
+                count++;
+                System.out.print(count);
+                emp.printShortInfo();
+            }
+            i++;
+        }
+    }
+
+    public static boolean findEmployee(Employee employee) {
+        for (Employee emp : employees) {
+            if (emp == null) {
+                break;
+            }
+            if (emp.equals(employee)) {
+                System.out.println("сотрудник есть");
+                return true;
+            }
+        }
+        System.out.println("сотрудника нет");
+        return false;
+    }
+
+    //метод получения сотрудника по id
+    public static void getEmployeeThroughId(int id) {
+        for (Employee emp : employees) {
+            if (emp != null && emp.getId() == id) {
+                System.out.println(emp);
+            }
+        }
+    }
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,14 @@ public class Main {
         Employee employee2 = new Employee(" иванов иван иваныч ", 3, 150);
         Employee employee3 = new Employee(" петров петр петрович ", 4, 200);
         Employee employee4 = new Employee(" семенов семен семенович ", 1, 450);
-        Employee employee5 = new Employee(" васечкин василий васильевич ", 2, 300);
+        Employee employee5 = new Employee(" васечкин василий васильевич ", 4, 300);
+        Employee employee6 = new Employee(" аннушка разлила масло ",4,450);
+        Employee employee7 = new Employee(" пушкин александр сергеевич ", 3, 270);
+        Employee employee8 = new Employee(" деревянный буратино карлович ", 1, 170);
+        Employee employee9 = new Employee(" григорьев григорий григорьевич ", 2, 230);
+        Employee employee10 = new Employee(" антонов антон антонович ", 5, 310);
+        Employee employee11 = new Employee(" сергеев сергей сергеевич ", 2, 400);
+
 
         System.out.println(employee1);
         employee1.printShortInfo();
@@ -20,11 +27,30 @@ public class Main {
         EmployeeBook.addToArray(employee3);
         EmployeeBook.addToArray(employee4);
         EmployeeBook.addToArray(employee5);
+        EmployeeBook.addToArray(employee6);
+        EmployeeBook.addToArray(employee7);
+        EmployeeBook.addToArray(employee8);
+        EmployeeBook.addToArray(employee9);
+        EmployeeBook.addToArray(employee10);
+        EmployeeBook.addToArray(employee11);
+
+
         EmployeeBook.listAllEmployees(EmployeeBook.getEmployees());
 
         EmployeeBook.calculateAverageSalary(EmployeeBook.getEmployees());
 
         EmployeeBook.calculateTheTax(EmployeeBook.getEmployees());
+
+        EmployeeBook.indexTheSalary(4,10);
+
+        EmployeeBook.printFirstEmployeeMaxSalary(4,200);
+
+        EmployeeBook.getEmployeesSalaryLowWage(300,2);
+
+        EmployeeBook.findEmployee(employee3);
+        EmployeeBook.findEmployee(employee11);
+
+        EmployeeBook.getEmployeeThroughId(4);
 
     }
 }
